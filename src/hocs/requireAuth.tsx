5b46658d774202c3a0c02f67fda4031df91181f0
Anasilvia Salazar <anasilvia82@gmail.com>
2021-11-19 09:53:13 -0600

    Changing cookies with token

--- src/hocs/requireAuth.tsx
+++ src/hocs/requireAuth.tsx
@@ -29,7 +29,7 @@ export function RequireAuth({ children }: { children: JSX.Element }) {
     // Redirect them to the /login page, but save the current location they were
     // trying to go to when they were redirected. This allows us to send them
     // along to that page after they login, which is a nicer user experience
-    // than dropping them off on the home page.
+    // than dropping them off on the home page
     return <Navigate to={LogInRoute.link()} state={{ from: location }} />;
   }
 
